e1a87c89cf325c075e34b65621f645573536ca7f
Helder Siqueira <helder.erik.he@gmail.com>
2021-04-17 19:38:22 -0300

    Versão finalizada

---
+++ main.js
@@ -0,0 +1,40 @@
+'mode strict'
+
+const links = {
+    "nome": "Hélder Siqueira",
+    "img": "avatar.png",
+    "links": [
+        {
+            "titulo": "site pessoal",
+            "url": "http://www.fernandoleonid.com.br/"
+        },
+        {
+            "titulo": "youtube",
+            "url": "https://www.youtube.com/fernandoleonid"
+        },
+        {
+            "titulo": "linkedin",
+            "url": "https://www.linkedin.com/in/fernandoleonid/"
+        },
+        {
+            "titulo": "twitter",
+            "url": "https://twitter.com/fernandoleonid"
+        }
+    ]
+}
+
+const avatar = document.getElementById('avatar');
+avatar.src = links.img;
+
+const titulo = document.getElementById('titulo'); 
+titulo.textContent = links.nome;
+
+    const div = document.getElementById('linkTree');
+
+for (let index = 0; index < links["links"].length; index++) {
+    
+    const link = document.createElement('a');
+    link.href = links["links"][index]["url"];
+    link.innerText =  links["links"][index]["titulo"];
+    div.appendChild(link);
+}
